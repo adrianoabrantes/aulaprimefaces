712d26324ff709bd3f4359dbe05855f2b38455ca
adrianoabrantes <devadrianoabrantes@icloud.com>
2019-08-02 01:40:48 -0300

    Arquivo de permissoes iniciado

---
+++ aulasjsfprimefaces/src/br/com/framework/util/UtilFramework.java
@@ -0,0 +1,15 @@
+package br.com.framework.util;
+
+import java.io.Serializable;
+
+public class UtilFramework implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private static ThreadLocal<Long> threadLocal = new ThreadLocal<Long>();
+
+	public synchronized static ThreadLocal<Long> getThreadLocal() {
+		return threadLocal;
+	}
+	
+}
